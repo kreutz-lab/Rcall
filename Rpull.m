dc426941d3092a9996451b44185e4bab6288da2f
srv-kreutz <srv-kreutz@IMBI-F-20001.ad.uniklinik-freiburg.de>
2023-01-28 10:26:31 +0100

    sprintf cannot handle \P in path

--- Rpull.m
+++ Rpull.m
@@ -65,9 +65,9 @@ if ~isempty(cmdout)
 end
 if status~=0
     if isfield(OPENR,'myLibPath')
-        error(sprintf(['Is your R path "' OPENR.Rexe '" correct? You can set the Rpath in Rinit(Rlibraries,Rpath). /n Is your R library path "' OPENR.myLibPath '"correct? You can set it in Rinit(Rlibraries,Rpath,Rlibpaths).']))
+        error(['Is your R path "' OPENR.Rexe '" correct? You can set the Rpath in Rinit(Rlibraries,Rpath). Is your R library path "' OPENR.myLibPath '"correct? You can set it in Rinit(Rlibraries,Rpath,Rlibpaths).'])
     else
-        error(sprintf(['Is your R path "' OPENR.Rexe '" correct? You can set the Rpath in Rinit(Rlibraries,Rpath). /n Is the R library path correct? You can set it in Rinit(Rlibraries,Rpath,Rlibpaths).']))
+        error(['Is your R path "' OPENR.Rexe '" correct? You can set the Rpath in Rinit(Rlibraries,Rpath). } Is the R library path correct? You can set it in Rinit(Rlibraries,Rpath,Rlibpaths).'])
     end
 end
 try
